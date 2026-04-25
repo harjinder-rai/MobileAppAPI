dac3d68112c3d3f65416c8f72555ec7073859042
manishrai1999 <harjinder1991@gmail.com>
2026-04-25 22:28:59 +0530

    remove cron

--- KalyanAutoUpdateResult.js
+++ KalyanAutoUpdateResult.js
@@ -270,6 +270,7 @@ async function updateKalyanResults() {
 if (require.main === module) {
   updateKalyanResults()
     .then((summary) => {
+      console.log(`Updated ${summary.updatedCount} Kalyan results in MongoDB`);
       console.log("Updated results:", summary.updatedResults);
     })
     .catch((error) => {
